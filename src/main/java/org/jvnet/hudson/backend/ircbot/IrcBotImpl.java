ffedb05bdf36789431ed13daa6ee718648f94eab
kohsuke <kohsuke@281f4120-3fa0-11df-aa16-63b581e3290d>
2009-10-10 01:34:40 +0000

    added an IRC bot to delegate some of the project operation work to the community
    git-svn-id: file:///home/kohsuke/ws/infradna/git-svn-bridge/hudson-repo/trunk/hudson/backend/ircbot@22592 281f4120-3fa0-11df-aa16-63b581e3290d

---
+++ src/main/java/org/jvnet/hudson/backend/ircbot/IrcBotImpl.java
@@ -0,0 +1,112 @@
+package org.jvnet.hudson.backend.ircbot;
+
+import org.jibble.pircbot.PircBot;
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.User;
+import org.kohsuke.jnt.JavaNet;
+import org.kohsuke.jnt.ProcessingException;
+import org.kohsuke.jnt.JNIssueComponent;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/**
+ * IRC Bot on irc.freenode.net as a means to delegate administrative work to committers.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class IrcBotImpl extends PircBot {
+    public IrcBotImpl() {
+        setName("hudson-admin");
+    }
+
+    @Override
+    protected void onMessage(String channel, String sender, String login, String hostname, String message) {
+        if (!channel.equals("#hudson"))     return; // not in this channel
+
+        String prefix = getNick() + ":";
+        if (!message.startsWith(prefix))  return;   // not send to me
+
+        String payload = message.substring(prefix.length(), message.length()).trim().toLowerCase();
+
+        Matcher m = Pattern.compile("(?:make|give|grant) (.+) (a )?(committer|commit access).*").matcher(payload);
+        if (m.matches()) {
+            grantCommitAccess(channel, sender, m.group(1));
+            return;
+        }
+
+        m = Pattern.compile("(?:create|make) (.+)(?: component)? in (?:issue|bug)(?: tracker| database)? for (.+)").matcher(payload);
+        if (m.matches()) {
+            createComponent(channel, sender, m.group(1), m.group(2));
+            return;
+        }
+
+        sendMessage(channel,"I didn't understand the command");
+    }
+
+    /**
+     * Is the sender respected in the channel?
+     *
+     * IOW, does he have a voice of a channel operator?
+     */
+    private boolean isSenderAuthorized(String sender, String channel) {
+        for (User u : getUsers(channel)) {
+            if (u.getNick().equals(sender)) {
+                String p = u.getPrefix();
+                if (p.contains("@") || p.contains("+"))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Grant commit access to a new user.
+     */
+    private void grantCommitAccess(String channel, String sender, String id) {
+        if (!isSenderAuthorized(channel,sender)) {
+            sendMessage(channel,"Only people with + or @ can run this command.");
+            return;
+        }
+
+        sendMessage(channel,"Making "+id+" a committer");
+        try {
+            JavaNet jn = JavaNet.connect();
+            jn.getProject("hudson").getMembership().grantRole(jn.getUser(id),"Developer");
+            jn.getProject("maven2-repository").getMembership().grantRole(jn.getUser(id),"javatools>maven2-repository.Maven Publisher");
+            sendMessage(channel,id +" is now a committer");
+        } catch (ProcessingException e) {
+            sendMessage(channel,"Failed to make "+id+" a committer: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates an issue tracker component.
+     */
+    private void createComponent(String channel, String sender, String subcomponent, String owner) {
+        if (!isSenderAuthorized(channel,sender)) {
+            sendMessage(channel,"Only people with + or @ can run this command.");
+            return;
+        }
+
+        sendMessage(channel,String.format("Adding a new subcomponent %s to the bug tracker, owned by %s",subcomponent,owner));
+        try {
+            JavaNet jn = JavaNet.connect();
+            JNIssueComponent comp = jn.getProject("hudson").getIssueTracker().getComponent("hudson");
+            comp.add(subcomponent,subcomponent+" plugin",owner,owner);
+            sendMessage(channel,"New component created");
+        } catch (ProcessingException e) {
+            sendMessage(channel,"Failed to create a new component: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, IrcException {
+        IrcBotImpl bot = new IrcBotImpl();
+        bot.connect("irc.freenode.net");
+        bot.setVerbose(true);
+        bot.joinChannel("#hudson");
+    }
+}
