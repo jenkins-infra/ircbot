9f2407a84f145f94c13a13b5569098c99be742be
R. Tyler Croy <tyler@monkeypox.org>
2015-12-22 14:56:49 -0800

    Add the #jenkins-community channel for the ircbot
    References INFRA-377

--- src/main/java/org/jenkinsci/backend/ircbot/IrcBotConfig.java
+++ src/main/java/org/jenkinsci/backend/ircbot/IrcBotConfig.java
@@ -24,8 +24,8 @@ public class IrcBotConfig {
     private static final String DEFAULT_IRCBOT_NAME = ("ircbot-"+System.getProperty("user.name"));
     static String NAME = System.getProperty(varPrefix+"name", DEFAULT_IRCBOT_NAME);
     static String SERVER = System.getProperty(varPrefix+"server", "irc.freenode.net");
-    static final Set<String> DEFAULT_CHANNELS = new HashSet<String>(Arrays.asList("#jenkins","#jenkins-infra"));
-    static final String CHANNELS_LIST = System.getProperty(varPrefix+"channels", "#jenkins,#jenkins-infra");
+    static final Set<String> DEFAULT_CHANNELS = new HashSet<String>(Arrays.asList("#jenkins", "#jenkins-infra", "#jenkins-community"));
+    static final String CHANNELS_LIST = System.getProperty(varPrefix+"channels", "#jenkins,#jenkins-infra,#jenkins-community");
 
     // IRC Hook
     static String IRC_HOOK_NAME = System.getProperty(varPrefix+"ircHook.name", "irc");
