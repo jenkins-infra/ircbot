f9f1c153f48596852e1830e8547c447b18607f99
Alex Earl <slide.o.mix@gmail.com>
2022-07-26 08:13:04 -0700

    Remove hosting code from IRC bot (#108)

--- src/main/java/org/jenkinsci/backend/ircbot/hosting/BuildSystemVerifier.java
+++
@@ -1,9 +0,0 @@
-package org.jenkinsci.backend.ircbot.hosting;
-
-import com.atlassian.jira.rest.client.api.domain.Issue;
-
-import java.io.IOException;
-
-public interface BuildSystemVerifier extends Verifier {
-    boolean hasBuildFile(Issue issue) throws IOException;
-}
